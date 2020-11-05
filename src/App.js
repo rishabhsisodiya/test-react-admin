09e4547286171fa10b5c0641eb8aba7769eaf6f7
Rishabh <rishabh.sisodiya4@gmail.com>
2020-11-06 04:36:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { Admin, Resource } from "react-admin";
+import jsonServerProvider from "ra-data-json-server";
+import { UserList } from "./Admin/Component/Users";
+import { PostCreate, PostEdit, PostList } from "./Admin/Component/Posts";
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
+import authProvider from "./Admin/Component/authProvider";
+import Dashboard from "./Admin/Component/Dashboard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
+
+const App = () => (
+  <Admin  dashboard={Dashboard} dataProvider={dataProvider} authProvider={authProvider}>
+    {/* Fetch users from api ~ https://jsonplaceholder.typicode.com/users */}
+    {/* ListGuesser dumps data in console. You can check custom methods here*/}
+    {/* <Resource name="users" list={ListGuesser} /> */}
+    <Resource name="users" list={UserList} icon={UserIcon}/> 
+    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon}/>
+  </Admin>
+);
 
 export default App;
